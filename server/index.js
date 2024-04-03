5ae38f568f7752506d1a41f409ba4368cdf43de9
Rodrigo Laiola Guimarães <rlaiola@gmail.com>
2024-04-02 21:49:49 -0300

    Add cluster

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ const rateLimit = require('express-rate-limit')
 const bodyParser = require('body-parser')
 const path = require('path')
 const puppeteer = require('puppeteer')
+const { Cluster } = require('puppeteer-cluster')
 // const logger = require('./logger')
 
 // Remove the X-Powered-By header
@@ -40,7 +41,7 @@ relaxApp.disable('x-powered-by')
 // Creating a limiter by calling rateLimit function with options:
 // max contains the maximum number of request and windowMs
 // contains the time in millisecond so only max amount of
-// request can be made in windowMS time.
+// request can be made in windowMs time.
 const limiterRelax = rateLimit({
   max: 50,
   windowMs: 1 * 60 * 1000, // 1 minute
@@ -58,144 +59,142 @@ apiApp.use(limiterRelax)
 apiApp.use(bodyParser.json())
 apiApp.use(limiterAPI)
 
-async function processAPIRequest (source, id, filename, index, query) {
-  // console.log("loadResults")
-  // logger.info("loadResults")
-  let json = {}
-
-  const browser = await puppeteer.launch({
-    // Running Puppeteer on a Docker container requires some additional dependencies
-    // https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#running-puppeteer-in-docker
-    args: [
-      '--no-sandbox',
-    ],
-    headless: 'new'
+;(async () => {
+  const cluster = await Cluster.launch({
+    puppeteerOptions: {
+      args: [
+        '--no-sandbox',
+      ],
+      headless: 'new'
+    },
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: 100,
   })
 
-  try {
-    const page = await browser.newPage()
+  await cluster.task(async ({ page, data: [source, id, filename, index, query] }) => {
+    // console.log("loadResults")
+    // logger.info("loadResults")
+    let json = {}
 
-    // Increases GitHub rate limit for API requests using Basic Authentication
-    // https://docs.github.com/en/rest/overview/resources-in-the-rest-api
-    // https://docs.github.com/en/rest/guides/getting-started-with-the-rest-api
-    if (process.env.GITHUB_ACCESS_TOKEN) {
-      await page.setExtraHTTPHeaders({
-        Authorization: 'token ' + process.env.GITHUB_ACCESS_TOKEN
-      })
-    }
-
-    let urlPath = ''
-    if (filename !== undefined && index !== undefined) {
-      urlPath = source + '/' + id + '/' + filename + '/' + index
-    } else urlPath = source + '/' + id
-
-    urlPath = urlPath + '?query=' + query
-    // console.log(urlPath)
-    // console.log('http://127.0.0.1:' + relaxPort + '/relax/api/' + urlPath)
-    // logger.info(urlPath)
-    // logger.info('http://127.0.0.1:' + relaxPort + '/relax/api/' + urlPath)
-    await page.goto('http://127.0.0.1:' + relaxPort + '/relax/api/' + urlPath, {
-      // Wait as much as necessary
-      timeout: 0
-    })
-
-    json = await page.evaluate(() => {
-      const value1 = document.getElementById('success').firstChild.nodeValue
-      const value2 = document.getElementById('query').firstChild.nodeValue
-      const value3 = document.getElementById('result').firstChild.nodeValue
-
-      return {
-        success: value1,
-        query: value2,
-        result: value3
+    try {
+      // Increases GitHub rate limit for API requests using Basic Authentication
+      // https://docs.github.com/en/rest/overview/resources-in-the-rest-api
+      // https://docs.github.com/en/rest/guides/getting-started-with-the-rest-api
+      if (process.env.GITHUB_ACCESS_TOKEN) {
+        await page.setExtraHTTPHeaders({
+          Authorization: 'token ' + process.env.GITHUB_ACCESS_TOKEN
+        })
       }
-    })
 
-    // Close it
-    await page.close()
-  } catch (err) {
-    console.log(err);
-    json = {
-      success: false,
-      error: err.stack
+      let urlPath = ''
+      if (filename !== undefined && index !== undefined) {
+        urlPath = source + '/' + id + '/' + filename + '/' + index
+      } else urlPath = source + '/' + id
+
+      urlPath = urlPath + '?query=' + query
+      // console.log(urlPath)
+      // console.log('http://127.0.0.1:' + relaxPort + '/relax/api/' + urlPath)
+      // logger.info(urlPath)
+      // logger.info('http://127.0.0.1:' + relaxPort + '/relax/api/' + urlPath)
+      await page.goto('http://127.0.0.1:' + relaxPort + '/relax/api/' + urlPath, {
+        // Wait as much as necessary
+        timeout: 0
+      })
+
+      json = await page.evaluate(() => {
+        const value1 = document.getElementById('success').firstChild.nodeValue
+        const value2 = document.getElementById('query').firstChild.nodeValue
+        const value3 = document.getElementById('result').firstChild.nodeValue
+
+        return {
+          success: value1,
+          query: value2,
+          result: value3
+        }
+      })
+    } catch (err) {
+      console.log(err);
+      json = {
+        success: false,
+        error: err.stack
+      }
     }
-  } finally {
-    await browser.close();
-  }
 
-  console.log(json)
-  return json
-}
+    console.log(json)
+    return json
+  })
 
-// Handling URL path with filename and index
-apiApp.get('/relax/api/:source/:id/:filename/:index', async function (req, res) {
-  // console.log("Handling URL path with filename and index")
-  // console.log(req.params)
-  // console.log(req.query)
+  // Handling URL path with filename and index
+  apiApp.get('/relax/api/:source/:id/:filename/:index', async function (req, res) {
+    // console.log("Handling URL path with filename and index")
+    // console.log(req.params)
+    // console.log(req.query)
 
-  // logger.info("Handling URL path with filename and index")
-  // logger.info(req.params)
-  // logger.info(req.query)
+    // logger.info("Handling URL path with filename and index")
+    // logger.info(req.params)
+    // logger.info(req.query)
 
-  const { source, id, filename, index } = req.params
-  const query = req.query.query
+    const { source, id, filename, index } = req.params
+    const query = req.query.query
 
-  const jsonResponse = await processAPIRequest(source, id, filename, index, query)
-  if (jsonResponse.success === false && jsonResponse.error) {
-    res.status(500).json(jsonResponse)
-    process.exit(1);
-  } else {
-    res.json(jsonResponse)
-  }
-})
+    const jsonResponse = await cluster.execute([source, id, filename, index, query])
+    if (jsonResponse.success === false && jsonResponse.error) {
+      res.status(500).json(jsonResponse)
+      process.exit(1);
+    } else {
+      res.json(jsonResponse)
+    }
+  })
 
-// Handling URL path with source and id only
-apiApp.get('/relax/api/:source/:id', async function (req, res) {
-  // console.log("Handling URL path with source and id only")
-  // console.log(req.params)
-  // console.log(req.query)
+  // Handling URL path with source and id only
+  apiApp.get('/relax/api/:source/:id', async function (req, res) {
+    // console.log("Handling URL path with source and id only")
+    // console.log(req.params)
+    // console.log(req.query)
 
-  // logger.info("Handling URL path with source and id only")
-  // logger.info(req.params)
-  // logger.info(req.query)
+    // logger.info("Handling URL path with source and id only")
+    // logger.info(req.params)
+    // logger.info(req.query)
 
-  const { source, id } = req.params
-  const query = req.query.query
+    const { source, id } = req.params
+    const query = req.query.query
 
-  const jsonResponse = await processAPIRequest(source, id, undefined, undefined, query)
-  if (jsonResponse.success === false && jsonResponse.error) {
-    res.status(500).json(jsonResponse)
-    process.exit(1);
-  } else {
-    res.json(jsonResponse)
-  }
-})
+    const jsonResponse = await cluster.execute([source, id, undefined, undefined, query])
+    if (jsonResponse.success === false && jsonResponse.error) {
+      res.status(500).json(jsonResponse)
+      process.exit(1);
+    } else {
+      res.json(jsonResponse)
+    }
+  })
 
-// Handling all other URLs by returning an empty json
-apiApp.get('*', (req, res) => {
-  // console.log("Handling all other URLs by returning an empty json")
-  // logger.info("Handling all other URLs by returning an empty json")
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ }))
-})
+  // Handling all other URLs by returning an empty json
+  apiApp.get('*', (req, res) => {
+    // console.log("Handling all other URLs by returning an empty json")
+    // logger.info("Handling all other URLs by returning an empty json")
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ }))
+  })
 
-// Handling RelaX routing in production
-// https://www.pluralsight.com/guides/handling-react-routing-in-production
-relaxApp.get('/*', function (req, res) {
-  // console.log("Handling RelaX routing in production")
-  // logger.info("Handling RelaX routing in production")
-  res.sendFile(path.join(__dirname, '../dist/relax', 'index.html'))
-})
+  // Handling RelaX routing in production
+  // https://www.pluralsight.com/guides/handling-react-routing-in-production
+  relaxApp.get('/*', function (req, res) {
+    // console.log("Handling RelaX routing in production")
+    // logger.info("Handling RelaX routing in production")
+    res.sendFile(path.join(__dirname, '../dist/relax', 'index.html'))
+  })
 
-const apiPort = process.env.RELAX_API_PORT || 3000
-const relaxPort = process.env.RELAX_PORT || 8080
+  const apiPort = process.env.RELAX_API_PORT || 3000
+  const relaxPort = process.env.RELAX_PORT || 8080
 
-apiApp.listen(apiPort, () => {
-  console.log('RelaX API listening on port', apiPort)
-  // logger.info('RelaX API listening on port', apiPort)
-})
+  apiApp.listen(apiPort, () => {
+    console.log('RelaX API listening on port', apiPort)
+    // logger.info('RelaX API listening on port', apiPort)
+  })
 
-relaxApp.listen(relaxPort, () => {
-  console.log('RelaX Web application listening on port', relaxPort)
-  // logger.info('RelaX Web application listening on port', relaxPort)
-})
+  relaxApp.listen(relaxPort, () => {
+    console.log('RelaX Web application listening on port', relaxPort)
+    // logger.info('RelaX Web application listening on port', relaxPort)
+  })
+
+})()
